cdaa3f9ef743e60ff448874fafb1600d74026265
NorthPaddy <believe.you.trust.you@gmail.com>
2020-11-21 14:33:13 +0900

    NextPage class作成(#11)

--- lib/next_page.dart
+++ lib/next_page.dart
@@ -1,1 +1,33 @@
+import 'package:flutter/material.dart';
 
+class NextPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("次のページ"),
+        actions: [
+          Icon(Icons.adb_outlined),
+        ],
+      ),
+      body: Container(
+        color: Colors.red,
+        child: Center(
+            child: Column(mainAxisSize: MainAxisSize.min,
+                children: [
+                  Text('遷移しましたっ！',
+                      style: TextStyle(
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold,
+                      )),
+        ])),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.arrow_back),
+        onPressed: () {
+          Navigator.pop(context);
+        },
+      ),
+    );
+  }
+}
