de8f3e71c215aec19e105b04c07c58ff53afec8e
NorthPaddy <believe.you.trust.you@gmail.com>
2020-11-26 20:01:17 +0900

    メインページのリスト化(#28)

--- lib/main.dart
+++ lib/main.dart
@@ -9,121 +9,74 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final title = 'フラッターデモホームページ';
+
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+        debugShowCheckedModeBanner: false,
+        title: title,
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        home: MainPage());
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  bool favorite;
-
+class MainPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("フラッターデモホームページ"),
-        actions: [
-          Icon(Icons.adb_outlined),
-        ],
+        title: Text('フラッターデモホームページ'),
       ),
-      body: Container(
-        child: Center(
-          child: Column(mainAxisSize: MainAxisSize.min, children: [
-            // Image.network(
-            //   'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg',
-            // ),
-            Image.asset(
-              'images/owl.jpg',
-              height: 250,
-              width: 250,
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 20),
+        child: ListView(
+          children: <Widget>[
+            ListTile(
+              contentPadding: EdgeInsets.all(10.0),
+              leading: Image.asset(
+                'images/owl.jpg',
+              ),
+              title: Text('フクロウ'),
+              trailing: Icon(Icons.arrow_forward_ios),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => NextPage()),
+                );
+              },
             ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                RichText(
-                  textScaleFactor: MediaQuery.of(context).textScaleFactor,
-                  text: TextSpan(
-                    style: TextStyle(color: Colors.black),
-                    children: <TextSpan>[
-                      TextSpan(text: 'いいね！した回数は '),
-                      TextSpan(
-                          text: '$_counter',
-                          style: Theme.of(context).textTheme.headline4),
-                      TextSpan(text: ' 回'),
-                    ],
-                  ),
-                ),
-              ],
+            ListTile(
+              contentPadding: EdgeInsets.all(10.0),
+              leading: Image.asset(
+                'images/owl.jpg',
+              ),
+              title: Text('フクロウ'),
+              trailing: Icon(Icons.arrow_forward_ios),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => NextPage()),
+                );
+              },
             ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                FlatButton(
-                  child: Icon(
-                    favorite == true ? Icons.favorite : Icons.favorite_border,
-                    color: favorite == true ? Colors.red : Colors.grey,
-                  ),
-                  onPressed: () {
-                    setState(() {
-                      if (favorite != true) {
-                        //ハートが押されたときにfavoriteにtrueを代入している
-                        favorite = true;
-                      } else {
-                        favorite = false;
-                      }
-                    });
-                  },
-                ),
-                RaisedButton.icon(
-                  icon: Icon(
-                    Icons.sentiment_satisfied_alt_outlined,
-                    color: Colors.white,
-                  ),
-                  label: Text('いいね！'),
-                  color: Colors.blue,
-                  textColor: Colors.white,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  onPressed: _incrementCounter,
-                ),
-              ],
+            ListTile(
+              contentPadding: EdgeInsets.all(10.0),
+              leading: Image.asset(
+                'images/owl.jpg',
+              ),
+              title: Text('フクロウ'),
+              trailing: Icon(Icons.arrow_forward_ios),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => NextPage()),
+                );
+              },
             ),
-          ]),
+          ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.arrow_forward),
-        onPressed: () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => NextPage()),
-          );
-        },
       ),
     );
   }
