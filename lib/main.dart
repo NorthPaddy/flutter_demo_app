df048e1b21042b49a815343875a20199a1dff77a
NorthPaddy <believe.you.trust.you@gmail.com>
2020-11-30 19:08:51 +0900

    List型を作りListViewにて呼び出した(#29)

--- lib/main.dart
+++ lib/main.dart
@@ -23,6 +23,19 @@ class MyApp extends StatelessWidget {
 }
 
 class MainPage extends StatelessWidget {
+  var imageLists = [
+    'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg',
+    'https://cdn.pixabay.com/photo/2017/06/09/09/39/adler-2386314__340.jpg',
+    'https://cdn.pixabay.com/photo/2014/05/20/21/20/bird-349026__340.jpg',
+    'https://cdn.pixabay.com/photo/2015/10/12/14/53/kingfisher-983944__340.jpg',
+  ];
+  var imageTitles = [
+    'フクロウ',
+    '鷲',
+    'ひよこ',
+    'カワセミ',
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,14 +44,19 @@ class MainPage extends StatelessWidget {
       ),
       body: Padding(
         padding: EdgeInsets.symmetric(horizontal: 20),
-        child: ListView(
-          children: <Widget>[
-            ListTile(
+        child: ListView.builder(
+          itemCount: imageLists.length,
+          itemBuilder: (context, int index) {
+            return ListTile(
               contentPadding: EdgeInsets.all(10.0),
-              leading: Image.asset(
-                'images/owl.jpg',
+              leading: Image.network(
+                '${imageLists[index]}',
+                height: 80,
+                width: 80,
               ),
-              title: Text('フクロウ'),
+              title: Text(
+                '${imageTitles[index]}',
+              ),
               trailing: Icon(Icons.arrow_forward_ios),
               onTap: () {
                 Navigator.push(
@@ -46,36 +64,8 @@ class MainPage extends StatelessWidget {
                   MaterialPageRoute(builder: (context) => NextPage()),
                 );
               },
-            ),
-            ListTile(
-              contentPadding: EdgeInsets.all(10.0),
-              leading: Image.asset(
-                'images/owl.jpg',
-              ),
-              title: Text('フクロウ'),
-              trailing: Icon(Icons.arrow_forward_ios),
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => NextPage()),
-                );
-              },
-            ),
-            ListTile(
-              contentPadding: EdgeInsets.all(10.0),
-              leading: Image.asset(
-                'images/owl.jpg',
-              ),
-              title: Text('フクロウ'),
-              trailing: Icon(Icons.arrow_forward_ios),
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => NextPage()),
-                );
-              },
-            ),
-          ],
+            );
+          },
         ),
       ),
     );
